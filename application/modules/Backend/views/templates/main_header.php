8467e70d0b94367765fda06fdf84566df2c15cc4
irfanputranto <putrantoirfan@gmail.com>
2020-12-20 20:12:30 +0700

    updateing template

--- application/modules/Backend/views/templates/main_header.php
+++ application/modules/Backend/views/templates/main_header.php
@@ -13,7 +13,7 @@
                           <span class=" fa fa-angle-down"></span>
                       </a>
                       <ul class="dropdown-menu dropdown-usermenu pull-right">
-                          <li><a href="javascript:;"> Profile</a></li>
+                          <!-- <li><a href="javascript:;"> Profile</a></li> -->
 
                           <li><a href="<?= base_url('logout'); ?>"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                       </ul>
